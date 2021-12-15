b45c88a4f429ca2e9bf96af26f77bc1ae2888ad4
Andrew Kara <a.kara701@gmail.com>
2021-12-16 00:30:44 +0200

    Done linq task

---
+++ Module3HW4/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace Module3HW4.Models;
+
+public class User
+{
+    private static int _id = 0;
+    public User()
+    {
+        _id++;
+        Id = _id;
+    }
+
+    public int Id { get; }
+    public string? FirstName { get; init; }
+    public string? LastName { get; init; }
+    public int Age { get; init; }
+    public string? PhoneNumber { get; init; }
+}
---
+++ Module3HW4/Program.cs
@@ -0,0 +1,100 @@
+﻿using Module3HW4.Models;
+
+namespace Module3HW4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var users = new User[]
+            {
+                new User()
+                {
+                    FirstName = "asd",
+                    LastName = "sad",
+                    Age = 12,
+                    PhoneNumber = "21343242342"
+                },
+                new User()
+                {
+                    FirstName = "gfd",
+                    LastName = "ert",
+                    Age = 54,
+                    PhoneNumber = "7856786"
+                },
+                new User()
+                {
+                    FirstName = "ghj",
+                    LastName = "yuoy",
+                    Age = 67,
+                    PhoneNumber = "46364364"
+                },
+                new User()
+                {
+                    FirstName = "oify",
+                    LastName = "gfegc",
+                    Age = 42,
+                    PhoneNumber = "546547457452"
+                },
+                new User()
+                {
+                    FirstName = "fdsbds",
+                    LastName = "shgerty",
+                    Age = 54,
+                    PhoneNumber = "68536456"
+                },
+                new User()
+                {
+                    FirstName = "fgdfsh",
+                    LastName = "nfghkdy",
+                    Age = 65,
+                    PhoneNumber = "46536436463"
+                },
+                new User()
+                {
+                    FirstName = "vczvbz",
+                    LastName = "dfhsdbvcxhdfhsdf",
+                    Age = 78,
+                    PhoneNumber = "23654634564"
+                }
+            }.ToList();
+
+            var users2 = new User[]
+            {
+                new User()
+                {
+                    FirstName = "fgdfsh",
+                    LastName = "shgerty",
+                    Age = 13,
+                    PhoneNumber = "754748765"
+                },
+                new User()
+                {
+                    FirstName = "ruteinb",
+                    LastName = "nfghkdy",
+                    Age = 14,
+                    PhoneNumber = "65853"
+                },
+                new User()
+                {
+                    FirstName = "vczvbz",
+                    LastName = "dfhsdbvcxhdfhsdf",
+                    Age = 17,
+                    PhoneNumber = "656435"
+                }
+            }.ToList();
+            var firstOrDefault = users.FirstOrDefault(user => user.Age > 30);
+            var where = users.Where(user => user.Age > 30);
+            var select = users.Select(user => user.Age);
+            var orderBy = users.OrderBy(user => user.FirstName);
+            var thenBy = orderBy.ThenBy(user => user.Age);
+            var join =
+                users.Join(
+                    users2,
+                    user => user.LastName,
+                    user2 => user2.LastName,
+                    (user, user2) =>
+                        new { User1Age = user.LastName, User2 = user2.LastName });
+        }
+    }
+}
